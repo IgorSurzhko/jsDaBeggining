45242e1e263be99241832a97088aa95bc4a8bfac
Ermygut <ermygut@gmail.com>
2021-03-04 13:34:26 +0200

    first JS lesson, basic

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
 "use strict";
 
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// console.log(numberOfFilms);
+
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
+};
+
+const a = prompt('Последний просмотренный фильм:', ''),
+	  b = prompt('На сколько оцените этот фильм?', ''),
+	  c = prompt('Последний просмотренный фильм:', ''),
+	  d = prompt('На сколько оцените этот фильм?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
 
