fae198086781e33e26385b631f7fdbc10d13f7d7
Ermygut <ermygut@gmail.com>
2021-03-04 17:15:30 +0200

    new lesson

--- js/script.js
+++ js/script.js
@@ -11,13 +11,31 @@ const personalMovieDB = {
 	privat: false
 };
 
-const a = prompt('Последний просмотренный фильм:', ''),
-	  b = prompt('На сколько оцените этот фильм?', ''),
-	  c = prompt('Последний просмотренный фильм:', ''),
-	  d = prompt('На сколько оцените этот фильм?', '');
+for (let i = 0; i < 2; i++) {
+	const a = prompt('Последний просмотренный фильм:', ''),
+	      b = prompt('На сколько оцените этот фильм?', '');
+	
+	if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+		personalMovieDB.movies[a] = b;	
+		console.log('done!');
+	} else {
+		console.log('error!');
+		i--;
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+	}	  
+
+}
 
 console.log(personalMovieDB);
 
+
+
+
+
+
+
+
+
+// for (let i = 1; i < 10; i++) {
+// 	console.log(i);
+// }
